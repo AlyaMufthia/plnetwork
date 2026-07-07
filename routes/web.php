1a84b98fa4053a717b41ef5949169426ce535f0d
Syarifah Chadijah <syarifah.chadijah06@gmail.com>
2026-07-07 10:00:08 +0700

    menghapus sidebar dan isi pengaturan

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RiwayatController;
 use App\Http\Controllers\LaporanController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PengaturanController;
 
 Route::get('/register',   fn() => view('register'))->name('register');
 Route::get('/login',      fn() => view('login'))->name('login');
 Route::get('/dashboard',  [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/laporan',    fn() => view('laporan'))->name('laporan.index');
 Route::post('/laporan',   [LaporanController::class, 'store'])->name('laporan.store');
-Route::get('/pengaturan', [PengaturanController::class, 'index'])->name('pengaturan.index');
-Route::post('/pengaturan',[PengaturanController::class, 'update'])->name('pengaturan.update');
 Route::get('/dashboard/chart-data',  [DashboardController::class, 'chartData'])->name('dashboard.chart-data');
 Route::get('/dashboard/status-data', [DashboardController::class, 'statusData'])->name('dashboard.status-data');
 
